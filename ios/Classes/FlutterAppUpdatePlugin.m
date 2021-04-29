96e3dd7bafee6f2c6f22f4eeef442b69f6adb604
azhon <zhongshiyuan@feng-go.com>
2021-04-29 14:57:15 +0800

    ios 修改为Object-C

--- ios/Classes/FlutterAppUpdatePlugin.m
+++ ios/Classes/FlutterAppUpdatePlugin.m
@@ -1,15 +1,26 @@
 #import "FlutterAppUpdatePlugin.h"
-#if __has_include(<flutter_app_update/flutter_app_update-Swift.h>)
-#import <flutter_app_update/flutter_app_update-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_app_update-Swift.h"
-#endif
 
 @implementation FlutterAppUpdatePlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterAppUpdatePlugin registerWithRegistrar:registrar];
+    
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+        methodChannelWithName:@"azhon_app_update"
+              binaryMessenger:[registrar messenger]];
+    FlutterAppUpdatePlugin* instance = [[FlutterAppUpdatePlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    if ([call.method isEqualToString:@"getVersionCode"]){
+        NSString *versionCode = NSBundle.mainBundle.infoDictionary[@"CFBundleVersion"];
+        result(@([versionCode intValue]));
+    } else if([call.method isEqualToString:@"getVersionName"]){
+        NSString *versionName = NSBundle.mainBundle.infoDictionary[@"CFBundleShortVersionString"];
+        result(versionName);
+    }else if([call.method isEqualToString:@"update"]){
+    
+    }else if([call.method isEqualToString:@"cancel"]){
+        NSLog(@"取消");
+    }
 }
 @end
